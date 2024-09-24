af9144c8cd627d6798bde0f43db1580541f73965
kareem-torky <kareemmfouad.dev@gmail.com>
2024-09-25 01:15:34 +0300

    fix

--- app/Jobs/Concerns/InteractsWithException.php
+++ app/Jobs/Concerns/InteractsWithException.php
@@ -14,6 +14,10 @@ trait InteractsWithException
 
     public bool $onlyLogWhenFail = true;
 
+    public bool $failIfDelayIsNull = true;
+
+    public bool $appendExceptionToContext = false;
+
     protected function handleException(Exception $exception, bool $fail = false, ?int $delay = null): void
     {
         if ($this->shouldLog && ! $this->onlyLogWhenFail) {
@@ -22,7 +26,14 @@ trait InteractsWithException
             );
         }
 
-        if ($fail || $this->isAttemptedTooManyTimes()) {
+        $delay ??= $this->getDelayInSeconds(
+            code: $exception->getCode(),
+        );
+
+        if ($this->shouldFail(
+            fail: $fail,
+            delay: $delay,
+        )) {
             if ($this->shouldLog && $this->onlyLogWhenFail) {
                 $this->logException(
                     exception: $exception,
@@ -36,39 +47,59 @@ trait InteractsWithException
             return;
         }
 
-        $delay ??= $this->getDelayInSeconds(
-            code: $exception->getCode(),
-        );
-
         $this->release(
             delay: $delay,
         );
     }
 
+    protected function getDelayInSeconds(int|string $code): ?int
+    {
+        return match ($code) {
+            401 => null,
+            429 => 60,
+            default => 0,
+        };
+    }
+
+    protected function shouldFail(bool $fail, ?int $delay): bool
+    {
+        if ($fail || $this->isAttemptedTooManyTimes()) {
+            return true;
+        }
+
+        if ($this->failIfDelayIsNull && $delay === null) {
+            return true;
+        }
+
+        return false;
+    }
+
     protected function isAttemptedTooManyTimes(): bool
     {
-        if ($this->attempts() > 250) {
+        if ($this->attempts() >= ($this->tries - 1)) {
             return true;
         }
 
         return $this->maxAttempts !== null && $this->attempts() >= $this->maxAttempts;
     }
 
-    protected function getDelayInSeconds(int $code): int
-    {
-        return match ($code) {
-            429 => 60,
-            default => 0,
-        };
-    }
-
     protected function logException(Exception $exception): void
     {
+        $context = method_exists(
+            object_or_class: $exception,
+            method: 'context',
+        ) ? $exception->context() : [];
+
+        $context = $this->appendExceptionToContext ? array_merge_recursive(
+            [
+                'exception' => $exception,
+            ],
+            $context,
+        ) : $context;
+
         logger()->error(
             message: $exception->getMessage(),
-            context: [
-                'code' => $exception->getCode(),
-            ],
+            context: $context,
         );
     }
 }
