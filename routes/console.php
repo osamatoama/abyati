394df76ee39d4b2355f23badc0d5105b6cd5477b
kareem-torky <kareemmfouad.dev@gmail.com>
2024-09-29 05:25:30 +0300

    db-backup-commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(
+    command: 'backup:clean'
+)->twiceDaily();
+
+Schedule::command(
+    command: 'backup:run --only-db'
+)->twiceDaily();
