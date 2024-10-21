80e19b5b813eb1e7a271a64d1b3f489bbd67c0c3
kareem-torky <kareemmfouad.dev@gmail.com>
2024-10-21 11:34:52 +0300

    employee/fix-barcode-scan-1

---
+++ resources/views/livewire/support/orders/archive/process-order.blade.php
@@ -0,0 +1,203 @@
+<div>
+    @if($executedItems->count())
+        <div class="mb-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">تم التأكيد ({{ $executedItems->count() }})</h3>
+                </div>
+
+                <div class="card-body">
+                    <div class="order-products">
+                        <div class="table-responsive">
+                            <table class="table table-row-bordered">
+                                <thead>
+                                    <th class="fw-bold">{{ __('support.products.attributes.image') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.name') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.variant') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.barcode') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.quantity') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.executed_quantity') }}</th>
+                                </thead>
+
+                                <tbody>
+                                    @foreach($executedItems as $item)
+                                        @php
+                                            $product = $item->product;
+                                            $variant = $item->variant;
+                                        @endphp
+
+                                        <tr>
+                                            <td>
+                                                <img src="{{ $product->main_image }}" alt="{{ $product->name }}" class="img-fluid" style="max-width: 50px;">
+                                            </td>
+                                            <td>{{ $product->name }}</td>
+                                            <td>
+                                                @if($variant?->optionValues?->isNotEmpty())
+                                                    {{ $variant->optionValues->map(fn($optionValue) => $optionValue->option->name . ': ' . $optionValue->name)->implode(' - ') }}
+                                                @else
+                                                    ---
+                                                @endif
+                                            </td>
+                                            <td>{{ $item->barcode ?? '---' }}</td>
+                                            <td>
+                                                {{ $item->quantity }}
+                                            </td>
+                                            <td>
+                                                {{ $item->executed_quantity }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    @if($toExecuteItems->count())
+        <div class="mb-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">قيد التأكيد ({{ $toExecuteItems->count() }})</h3>
+                </div>
+
+                <div class="card-body">
+                    <div class="order-products">
+                        <div class="table-responsive">
+                            <table class="table table-row-bordered">
+                                <thead>
+                                    <th class="fw-bold">{{ __('support.products.attributes.image') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.name') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.variant') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.barcode') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.quantity') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.executed_quantity') }}</th>
+                                </thead>
+
+                                <tbody>
+                                    @foreach($toExecuteItems as $item)
+                                        @php
+                                            $product = $item->product;
+                                            $variant = $item->variant;
+                                        @endphp
+
+                                        @if(filled($item->barcode))
+                                            <tr wire:key="to-execute-item-{{ $item->id }}">
+                                                <td>
+                                                    <img src="{{ $product->main_image }}" alt="{{ $product->name }}" class="img-fluid" style="max-width: 50px;">
+                                                </td>
+                                                <td>{{ $product->name }}</td>
+                                                <td>
+                                                    @if($variant?->optionValues?->isNotEmpty())
+                                                        {{ $variant->optionValues->map(fn($optionValue) => $optionValue->option->name . ': ' . $optionValue->name)->implode(' - ') }}
+                                                    @else
+                                                        ---
+                                                    @endif
+                                                </td>
+                                                <td>{{ $item->barcode ?? '---' }}</td>
+                                                <td>
+                                                    {{ $item->quantity }}
+                                                </td>
+                                                <td>
+                                                    {{ $item->executed_quantity }}
+                                                </td>
+                                            </tr>
+                                        @else
+                                            <tr>
+                                                <td>
+                                                    <img src="{{ $product->main_image }}" alt="{{ $product->name }}" class="img-fluid" style="max-width: 50px;">
+                                                </td>
+                                                <td>{{ $product->name }}</td>
+                                                <td>
+                                                    @if($variant?->optionValues?->isNotEmpty())
+                                                        {{ $variant->optionValues->map(fn($optionValue) => $optionValue->option->name . ': ' . $optionValue->name)->implode(' - ') }}
+                                                    @else
+                                                        ---
+                                                    @endif
+                                                </td>
+                                                <td colspan="4">
+                                                    <div class="alert alert-danger text-danger text-center fw-bold">
+                                                        <p class="mb-0">
+                                                            {{ __('support.orders.errors.no_barcode_for_this_product') }}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    @if($quantityIssuesItems->count())
+        <div class="mb-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">مشاكل كميات ({{ $quantityIssuesItems->count() }})</h3>
+                </div>
+
+                <div class="card-body">
+                    <div class="order-products">
+                        <div class="table-responsive">
+                            <table class="table table-row-bordered">
+                                <thead>
+                                    <th class="fw-bold">{{ __('support.products.attributes.image') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.name') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.variant') }}</th>
+                                    <th class="fw-bold">{{ __('support.products.attributes.barcode') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.quantity') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.executed_quantity') }}</th>
+                                    <th class="fw-bold">{{ __('support.orders.items.attributes.employee_note') }}</th>
+                                    <th></th>
+                                </thead>
+
+                                <tbody>
+                                    @foreach($quantityIssuesItems as $item)
+                                        @php
+                                            $product = $item->product;
+                                            $variant = $item->variant;
+                                        @endphp
+
+                                        <tr>
+                                            <td>
+                                                <img src="{{ $product->main_image }}" alt="{{ $product->name }}" class="img-fluid" style="max-width: 50px;">
+                                            </td>
+                                            <td>{{ $product->name }}</td>
+                                            <td>
+                                                @if($variant?->optionValues?->isNotEmpty())
+                                                    {{ $variant->optionValues->map(fn($optionValue) => $optionValue->option->name . ': ' . $optionValue->name)->implode(' - ') }}
+                                                @else
+                                                    ---
+                                                @endif
+                                            </td>
+                                            <td>{{ $item->barcode ?? '---' }}</td>
+                                            <td>
+                                                {{ $item->quantity }}
+                                            </td>
+                                            <td>
+                                                {{ $item->executed_quantity }}
+                                            </td>
+                                            <td>
+                                                {{ filled($item->employeeNote?->content) ? $item->employeeNote->content : '---' }}
+                                            </td>
+                                            <td>
+                                                <livewire:support.orders.complete-order-item :$item :key="'complete-' . $item->id" />
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
